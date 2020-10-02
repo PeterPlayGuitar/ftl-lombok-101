ff789a4a87b63e7691c72b405b22419d4a0cef47
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-10-02 22:07:00 +0400

    task006 - getter and setter example

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,6 @@
 import lombok.val;
 import tasks.CleanupExample;
+import tasks.GetterSetterExample;
 import tasks.nonNullExample.NonNullExample;
 import tasks.nonNullExample.Person;
 
@@ -7,6 +8,10 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        CleanupExample.run();
+        GetterSetterExample g = new GetterSetterExample();
+
+        System.out.println(g.getName());
+        g.setName("Vasya");
+        System.out.println(g);
     }
 }
