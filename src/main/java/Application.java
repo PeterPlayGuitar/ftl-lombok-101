d5b32f65e7959a32b6b8929f6b32b8027b6baa83
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-10-02 21:02:49 +0400

    task002 - val example

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,9 @@
+import tasks.ValExample;
+
 public class Application {
     public static void main(String[] args) {
-        
+        ValExample valExample = new ValExample();
+        System.out.println(valExample.example());
+        valExample.example2();
     }
 }
