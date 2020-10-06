93fe4d9b5836b911ed544b9c5db3b10320b9d2e0
Peter Zelinskiy <wiindon.mail@gmail.com>
2020-10-06 17:05:31 +0400

    task015 - getter lazy example

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,20 +1,20 @@
-import lombok.NonNull;
-import lombok.Synchronized;
 import lombok.val;
-import tasks.*;
-import tasks.nonNullExample.NonNullExample;
-import tasks.nonNullExample.Person;
-import tasks.nonNullExample.ToStringExample;
-
-import java.io.IOException;
-import java.util.Arrays;
+import lombok.var;
+import tasks.GetterLazyExample;
 
 public class Application {
 
     public static void main(String[] args) throws InterruptedException {
-        WithExample example1 = new WithExample("Peter", 12, 1.2);
-        System.out.println(example1.toString());
-        WithExample example2 =example1.withName("Vasya");
-        System.out.println(example2);
+        GetterLazyExample getterLazyExample = new GetterLazyExample();
+
+        long startTime = System.currentTimeMillis();
+        var data = getterLazyExample.getData();
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+
+        startTime = System.currentTimeMillis();
+        data = getterLazyExample.getData();
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
     }
 }
